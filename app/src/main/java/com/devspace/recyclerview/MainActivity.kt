58276d76e3b2b660708ee99b76b11c3bb8d0f3c5
Jusouza <jujussouza19@gmail.com>
2025-08-08 19:56:55 -0300

    Creating a contact list with recycler view, mocking data and changing UI state from list to grid.

--- app/src/main/java/com/devspace/recyclerview/MainActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/MainActivity.kt
@@ -1,10 +1,22 @@
 package com.devspace.recyclerview
 
+import android.media.Image
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+// criar data class - feito
+// criar a listagem a partir do data class - feito
+// criar um adapter - feito
+// setar um adapter feito
+// linear layout manager feito
+//submeter a lista
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +28,32 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val rvList = findViewById<RecyclerView>(R.id.rv_list)
+        val ivList = findViewById<ImageView>(R.id.ic_list)
+        val ivGrid = findViewById<ImageView>(R.id.ic_grid)
+        val adapter = ContactListAdapter()
+
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+        adapter.submitList(contacts)
+
+        ivGrid.setOnClickListener {
+            rvList.layoutManager = GridLayoutManager(this, 2)
+        }
+
+        ivList.setOnClickListener {
+            rvList.layoutManager = LinearLayoutManager(this)
+        }
+
+    }
+
+
+    fun changeToGrid(){
+
+    }
+
+    fun changeToList(){
+
     }
 }
